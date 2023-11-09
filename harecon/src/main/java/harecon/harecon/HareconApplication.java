51474f2ae4f5ffc6d33e6698e66ade016634b723
Helene Solhaug <54099085+h586613@users.noreply.github.com>
2023-11-09 14:50:49 +0100

    initialized spring boot project with web and amqp dependencies

---
+++ harecon/src/main/java/harecon/harecon/HareconApplication.java
@@ -0,0 +1,13 @@
+package harecon.harecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HareconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HareconApplication.class, args);
+	}
+
+}
---
+++ harecon/src/test/java/harecon/harecon/HareconApplicationTests.java
@@ -0,0 +1,13 @@
+package harecon.harecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HareconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
